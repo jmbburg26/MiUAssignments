7c12b0a40c4309a2c2db2846881280eeb235fa9c
John Brandenburg <jmbburg26@fullsail.edu>
2013-02-09 21:32:51 -0500

    Split app into Gold and Bronze.

---
+++ Homework Tracker App GOLD/js/json.js
@@ -0,0 +1,202 @@
+var json = {
+	"assignment1": {
+		"fname": ["First Name:", "Amanda"],
+		"lname": ["Last Name:", "Brandenburg"],
+		"email": ["Email:", "mrsjmbburg@gmail.com"],
+		"course": ["Course:", "SDI"],
+		"answer": ["Project Due:", "No"],
+		"date": ["Due Date:", "2013-03-15"],
+		"notes": ["Notes:","Are you enjoying School?"],
+		"complete": ["Hours to Complete:","1"]
+	},
+	"assignment2": {
+		"fname": ["First Name:", "John"],
+		"lname": ["Last Name:", "Brandenburg"],
+		"email": ["Email:", "jmbburg26@gmail.com"],
+		"course": ["Course:", "VFW"],
+		"answer": ["Project Due:", "Yes"],
+		"date": ["Due Date:", "2013-01-31"],
+		"notes": ["Notes:","Last project!!!"],
+		"complete": ["Hours to Complete:","8"]
+	},
+	"assignment3": {
+		"fname": ["First Name:", "Allan"],
+		"lname": ["Last Name:", "Bladorn"],
+		"email": ["Email:", "anb122410@gmail.com"],
+		"course": ["Course:", "PMA"],
+		"answer": ["Project Due:", "Unknown"],
+		"date": ["Due Date:", "2013-04-7"],
+		"notes": ["Notes:","Is it over?"],
+		"complete": ["Hours to Complete:","8"]
+	},
+	"assignment4": {
+		"fname": ["First Name:", "Doug"],
+		"lname": ["Last Name:", "Brandenburg"],
+		"email": ["Email:", "papabnsd@gmail.com"],
+		"course": ["Course:", "MMD"],
+		"answer": ["Project Due:", "Yes"],
+		"date": ["Due Date:", "2013-01-31"],
+		"notes": ["Notes:","Last project!!!"],
+		"complete": ["Hours to Complete:","8"]
+	},
+	"assignment5": {
+		"fname": ["First Name:", "Zach"],
+		"lname": ["Last Name:", "Brandenburg"],
+		"email": ["Email:", "zcbburg116@gmail.com"],
+		"course": ["Course:", "MMD2"],
+		"answer": ["Project Due:", "No"],
+		"date": ["Due Date:", "2013-02-28"],
+		"notes": ["Notes:","Last project!!!"],
+		"complete": ["Hours to Complete:","8"]
+	},
+	"assignment6": {
+		"fname": ["First Name:", "Sarah"],
+		"lname": ["Last Name:", "Brandenburg"],
+		"email": ["Email:", "jmbburg26@gmail.com"],
+		"course": ["Course:", "PMA2"],
+		"answer": ["Project Due:", "No"],
+		"date": ["Due Date:", "2013-01-31"],
+		"notes": ["Notes:","Last project!!!"],
+		"complete": ["Hours to Complete:","8"]
+	},
+	"assignment7": {
+		"fname": ["First Name:", "John"],
+		"lname": ["Last Name:", "Smith"],
+		"email": ["Email:", "jmbburg26@gmail.com"],
+		"course": ["Course:", "VFW"],
+		"answer": ["Project Due:", "Yes"],
+		"date": ["Due Date:", "2013-01-31"],
+		"notes": ["Notes:","Last project!!!"],
+		"complete": ["Hours to Complete:","8"]
+	},
+	"assignment8": {
+		"fname": ["First Name:", "John"],
+		"lname": ["Last Name:", "Doe"],
+		"email": ["Email:", "jmbburg26@gmail.com"],
+		"course": ["Course:", "PMA"],
+		"answer": ["Project Due:", "Yes"],
+		"date": ["Due Date:", "2013-01-31"],
+		"notes": ["Notes:","Last project!!!"],
+		"complete": ["Hours to Complete:","8"]
+	},
+	"assignment9": {
+		"fname": ["First Name:", "Jane"],
+		"lname": ["Last Name:", "Smith"],
+		"email": ["Email:", "jmbburg26@gmail.com"],
+		"course": ["Course:", "PMA2"],
+		"answer": ["Project Due:", "Yes"],
+		"date": ["Due Date:", "2013-01-31"],
+		"notes": ["Notes:","Last project!!!"],
+		"complete": ["Hours to Complete:","8"]
+	},
+	"assignment10": {
+		"fname": ["First Name:", "Jane"],
+		"lname": ["Last Name:", "Doe"],
+		"email": ["Email:", "jmbburg26@gmail.com"],
+		"course": ["Course:", "MMD"],
+		"answer": ["Project Due:", "Yes"],
+		"date": ["Due Date:", "2013-01-31"],
+		"notes": ["Notes:","Last project!!!"],
+		"complete": ["Hours to Complete:","8"]
+	},
+	"assignment11": {
+		"fname": ["First Name:", "John"],
+		"lname": ["Last Name:", "Brandenburg"],
+		"email": ["Email:", "jmbburg26@gmail.com"],
+		"course": ["Course:", "MMD2"],
+		"answer": ["Project Due:", "Yes"],
+		"date": ["Due Date:", "2013-01-31"],
+		"notes": ["Notes:","Last project!!!"],
+		"complete": ["Hours to Complete:","8"]
+	},
+	"assignment12": {
+		"fname": ["First Name:", "John"],
+		"lname": ["Last Name:", "Brandenburg"],
+		"email": ["Email:", "jmbburg26@gmail.com"],
+		"course": ["Course:", "VFW"],
+		"answer": ["Project Due:", "Yes"],
+		"date": ["Due Date:", "2013-01-31"],
+		"notes": ["Notes:","Last project!!!"],
+		"complete": ["Hours to Complete:","8"]
+	},
+	"assignment13": {
+		"fname": ["First Name:", "John"],
+		"lname": ["Last Name:", "Brandenburg"],
+		"email": ["Email:", "jmbburg26@gmail.com"],
+		"course": ["Course:", "VFW"],
+		"answer": ["Project Due:", "Yes"],
+		"date": ["Due Date:", "2013-01-31"],
+		"notes": ["Notes:","Last project!!!"],
+		"complete": ["Hours to Complete:","8"]
+	},
+	"assignment14": {
+		"fname": ["First Name:", "John"],
+		"lname": ["Last Name:", "Brandenburg"],
+		"email": ["Email:", "jmbburg26@gmail.com"],
+		"course": ["Course:", "VFW"],
+		"answer": ["Project Due:", "Yes"],
+		"date": ["Due Date:", "2013-01-31"],
+		"notes": ["Notes:","Last project!!!"],
+		"complete": ["Hours to Complete:","8"]
+	},
+	"assignment15": {
+		"fname": ["First Name:", "John"],
+		"lname": ["Last Name:", "Brandenburg"],
+		"email": ["Email:", "jmbburg26@gmail.com"],
+		"course": ["Course:", "VFW"],
+		"answer": ["Project Due:", "Yes"],
+		"date": ["Due Date:", "2013-01-31"],
+		"notes": ["Notes:","Last project!!!"],
+		"complete": ["Hours to Complete:","8"]
+	},
+	"assignment16": {
+		"fname": ["First Name:", "John"],
+		"lname": ["Last Name:", "Brandenburg"],
+		"email": ["Email:", "jmbburg26@gmail.com"],
+		"course": ["Course:", "VFW"],
+		"answer": ["Project Due:", "Yes"],
+		"date": ["Due Date:", "2013-01-31"],
+		"notes": ["Notes:","Last project!!!"],
+		"complete": ["Hours to Complete:","8"]
+	},
+	"assignment17": {
+		"fname": ["First Name:", "John"],
+		"lname": ["Last Name:", "Brandenburg"],
+		"email": ["Email:", "jmbburg26@gmail.com"],
+		"course": ["Course:", "VFW"],
+		"answer": ["Project Due:", "Yes"],
+		"date": ["Due Date:", "2013-01-31"],
+		"notes": ["Notes:","Last project!!!"],
+		"complete": ["Hours to Complete:","8"]
+	},
+	"assignment18": {
+		"fname": ["First Name:", "John"],
+		"lname": ["Last Name:", "Brandenburg"],
+		"email": ["Email:", "jmbburg26@gmail.com"],
+		"course": ["Course:", "VFW"],
+		"answer": ["Project Due:", "Yes"],
+		"date": ["Due Date:", "2013-01-31"],
+		"notes": ["Notes:","Last project!!!"],
+		"complete": ["Hours to Complete:","8"]
+	},
+	"assignment19": {
+		"fname": ["First Name:", "John"],
+		"lname": ["Last Name:", "Brandenburg"],
+		"email": ["Email:", "jmbburg26@gmail.com"],
+		"course": ["Course:", "VFW"],
+		"answer": ["Project Due:", "Yes"],
+		"date": ["Due Date:", "2013-01-31"],
+		"notes": ["Notes:","Last project!!!"],
+		"complete": ["Hours to Complete:","8"]
+	},
+	"assignment20": {
+		"fname": ["First Name:", "John"],
+		"lname": ["Last Name:", "Brandenburg"],
+		"email": ["Email:", "jmbburg26@gmail.com"],
+		"course": ["Course:", "VFW"],
+		"answer": ["Project Due:", "Yes"],
+		"date": ["Due Date:", "2013-01-31"],
+		"notes": ["Notes:","Last project!!!"],
+		"complete": ["Hours to Complete:","8"]
+	},
+}
